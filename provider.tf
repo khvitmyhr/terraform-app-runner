f2c9a08de282395e05cc0ada9fffba94305961b2
khvitmyhr <kzti@hotmail.com>
2024-10-15 09:38:30 +0000

    m

--- provider.tf
+++ provider.tf
@@ -5,9 +5,10 @@ terraform {
       version = "4.39.0"
     }
   }
+  
   backend "s3" {
     bucket = "pgr301-2024-terraform-state"
-    key    = "glenn.richard.bech/apprunner-a-new-state.state"
+    key    = "kihv/apprunner-a-new-state.state"
     region = "eu-west-1"
   }
 }
